f6d374ca68a08e6246ca74fbfaf9f082f208f762
Oskar Eriksson <oskaerik@gmail.com>
2019-02-22 10:07:12 +0100

    Create common

--- src/common.hpp
+++ src/common.hpp
@@ -1,10 +1,5 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <cassert>
-
-#define POP_SIZE 100
+#ifndef _COMMON_HPP_
+#define _COMMON_HPP_
 
 using Graph = std::vector<std::vector<double>>;
 
@@ -13,6 +8,7 @@ struct Solution {
     std::vector<int> delimiters; // size = m-1
 };
 
+// Generate a random solution
 Solution random_solution(int n, int m) {
     Solution s = { std::vector<int>(n-1), std::vector<int>(m-1) };
 
@@ -29,45 +25,7 @@ Solution random_solution(int n, int m) {
     return s;
 }
 
-Solution aex(Solution &p1, Solution &p2) {
-    // Create permutation mappings, from (index) -> to (value)
-    std::vector<int> map1(p1.sequence.size()+1);
-    std::vector<int> map2(p2.sequence.size()+1);
-    map1[p1.sequence.back()] = p1.sequence[0];
-    map2[p2.sequence.back()] = p2.sequence[0];
-    for (int i = 1; i < int(map1.size())-1; ++i) {
-        map1[p1.sequence[i-1]] = p1.sequence[i];
-        map2[p2.sequence[i-1]] = p2.sequence[i];
-    }
-
-    // Perform AEX (indicies 0 and 1 are ok)
-    Solution c = p1;
-    std::vector<bool> added(map1.size(), false);
-    added[c.sequence[0]] = added[c.sequence[1]] = true;
-    bool p1_turn = false;
-    for (int i = 2; i < int(p1.sequence.size()); ++i) {
-        int candidate = p1_turn ? map1[c.sequence[i-1]] : map2[c.sequence[i-1]];
-        if (added[candidate]) {
-            // Pick random non-added node
-            std::vector<int> remaining;
-            remaining.reserve(c.sequence.size());
-            for (int i = 1; i < int(added.size()); ++i)
-                if (!added[i]) remaining.push_back(i);
-            candidate = remaining[rand() % remaining.size()];
-        }
-        c.sequence[i] = candidate;
-        added[candidate] = true;
-        p1_turn = !p1_turn;
-    }
-
-    // Sanity check
-    assert(added[0] == false);
-    for (int i = 1; i < int(added.size()); ++i)
-        assert(added[i] == true);
-
-    return c;
-}
-
+// Swap two nodes in the sequence
 void mutation_swap(Solution &s) {
     int i = rand() % s.sequence.size();
     int j = i;
@@ -76,6 +34,7 @@ void mutation_swap(Solution &s) {
     std::swap(s.sequence[i], s.sequence[j]);
 }
 
+// Helper
 bool move_delimiter_left(Solution &s, int i) {
     if (s.delimiters[i] > 0
             && (i == 0 || s.delimiters[i-1] < s.delimiters[i])) {
@@ -85,6 +44,7 @@ bool move_delimiter_left(Solution &s, int i) {
     return false;
 }
 
+// Helper
 bool move_delimiter_right(Solution &s, int i) {
     if (s.delimiters[i] < int(s.sequence.size())
             && (i == int(s.delimiters.size())-1 || s.delimiters[i+1] > s.delimiters[i])) {
@@ -118,6 +78,7 @@ void mutation_delimiter(Solution &s) {
     }
 }
 
+// Print solution for debugging
 void print_solution(Solution &s) {
     printf("Vehicle 0: ");
     for (int j = 0; j < s.delimiters[0]; ++j)
@@ -137,39 +98,4 @@ void print_solution(Solution &s) {
     printf("\n\n");
 }
 
-int main() {
-    /*
-    Graph g = {
-        {0, 1, sqrt(2), 1},
-        {1, 0, 1, sqrt(2)},
-        {sqrt(2), 1, 0, 1},
-        {1, sqrt(2), 1, 0}
-    };
-    */
-    Graph g = {
-        {0, 1, sqrt(2), 1, sqrt(2), 1},
-        {1, 0, 1, sqrt(2), 1, sqrt(2)},
-        {sqrt(2), 1, 0, 1, 2, sqrt(5)},
-        {1, sqrt(2), 1, 0, sqrt(5), 2},
-        {sqrt(2), 1, 2, sqrt(5), 0, 1},
-        {1, sqrt(2), sqrt(5), 2, 1, 0}
-    };
-
-    int n = g.size(); // nodes
-    int m = 3; // vehicles
-    assert(n > 2);
-    assert(m > 1);
-    assert(m < n);
-
-    // Generate a random population
-    std::vector<Solution> population(POP_SIZE);
-    for (auto &s : population)
-        s = random_solution(n, m);
-
-    for (int i = 0; i < 10; ++i) {
-        print_solution(population[i]);
-        mutation_delimiter(population[i]);
-        print_solution(population[i]);
-        printf("\n\n");
-    }
-}
+#endif
---
+++ src/ga_main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cassert>
+#include "common.hpp"
+
+#define POP_SIZE 100
+
+Solution aex(Solution &p1, Solution &p2) {
+    // Create permutation mappings, from (index) -> to (value)
+    std::vector<int> map1(p1.sequence.size()+1);
+    std::vector<int> map2(p2.sequence.size()+1);
+    map1[p1.sequence.back()] = p1.sequence[0];
+    map2[p2.sequence.back()] = p2.sequence[0];
+    for (int i = 1; i < int(map1.size())-1; ++i) {
+        map1[p1.sequence[i-1]] = p1.sequence[i];
+        map2[p2.sequence[i-1]] = p2.sequence[i];
+    }
+
+    // Perform AEX (indicies 0 and 1 are ok)
+    Solution c = p1;
+    std::vector<bool> added(map1.size(), false);
+    added[c.sequence[0]] = added[c.sequence[1]] = true;
+    bool p1_turn = false;
+    for (int i = 2; i < int(p1.sequence.size()); ++i) {
+        int candidate = p1_turn ? map1[c.sequence[i-1]] : map2[c.sequence[i-1]];
+        if (added[candidate]) {
+            // Pick random non-added node
+            std::vector<int> remaining;
+            remaining.reserve(c.sequence.size());
+            for (int i = 1; i < int(added.size()); ++i)
+                if (!added[i]) remaining.push_back(i);
+            candidate = remaining[rand() % remaining.size()];
+        }
+        c.sequence[i] = candidate;
+        added[candidate] = true;
+        p1_turn = !p1_turn;
+    }
+
+    // Sanity check
+    assert(added[0] == false);
+    for (int i = 1; i < int(added.size()); ++i)
+        assert(added[i] == true);
+
+    return c;
+}
+
+int main() {
+    /*
+    Graph g = {
+        {0, 1, sqrt(2), 1},
+        {1, 0, 1, sqrt(2)},
+        {sqrt(2), 1, 0, 1},
+        {1, sqrt(2), 1, 0}
+    };
+    */
+    Graph g = {
+        {0, 1, sqrt(2), 1, sqrt(2), 1},
+        {1, 0, 1, sqrt(2), 1, sqrt(2)},
+        {sqrt(2), 1, 0, 1, 2, sqrt(5)},
+        {1, sqrt(2), 1, 0, sqrt(5), 2},
+        {sqrt(2), 1, 2, sqrt(5), 0, 1},
+        {1, sqrt(2), sqrt(5), 2, 1, 0}
+    };
+
+    int n = g.size(); // nodes
+    int m = 3; // vehicles
+    assert(n > 2);
+    assert(m > 1);
+    assert(m < n);
+
+    // Generate a random population
+    std::vector<Solution> population(POP_SIZE);
+    for (auto &s : population)
+        s = random_solution(n, m);
+
+    for (int i = 0; i < 10; ++i) {
+        print_solution(population[i]);
+        mutation_delimiter(population[i]);
+        print_solution(population[i]);
+        printf("\n\n");
+    }
+}
