4ffa5a9167c71d117467e5932c4cd846cd79607a
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2019-02-27 14:38:19 +0100

    Change to graph output

--- src/common.hpp
+++ src/common.hpp
@@ -15,20 +15,23 @@ using ms = std::chrono::microseconds;
 
 using Graph = std::vector<std::vector<double>>;
 
-Graph uniform_graph(std::ofstream &file, int n, int size = 10){
+Graph uniform_graph(int n){
   assert(n >= 3);
-  file << "Uniform graph, " << n << " nodes\n";
+  char file_name[64];
+  sprintf(file_name, "./out/graph_u_n%d.csv", n);
+  std::ofstream file(file_name);
+  file << "x,y\n";
 
   std::random_device rd;  // Will be used to obtain a seed for the random number engine
   std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
-  std::uniform_real_distribution<> dis(0, size); // Random uniform double distribution in [0, 10)
+  std::uniform_real_distribution<> dis(0, 100); // Random uniform double distribution in [0, 10)
 
   std::vector<std::pair<double,double>> nodes(n);
   for(int i = 0; i < n; i++) {
     double x = dis(gen);
     double y = dis(gen);
     nodes[i] = { x, y };
-    file << x << ' ' << y << '\n';
+    file << x << ", " << y << '\n';
   }
 
   Graph graph(n, std::vector<double>(n));
@@ -41,14 +44,17 @@ Graph uniform_graph(std::ofstream &file, int n, int size = 10){
   return graph;
 }
 
-Graph clustered_graph(std::ofstream &file, int n, int c, int sd, int size = 100) {
+Graph clustered_graph(int n, int c, int sd) {
   assert(n >= 3);
   assert(c >= 2);
-  file << "Clustered graph, " << n << " nodes, " << c << " clusters, " << sd << " stdv\n";
+  char file_name[64];
+  sprintf(file_name, "./out/graph_c_n%d_c%d_sd%d.csv", n, c, sd);
+  std::ofstream file(file_name);
+  file << "x,y\n";
 
   std::random_device rd;  // Will be used to obtain a seed for the random number engine
   std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
-  std::uniform_real_distribution<> cluster_dis(0, size); // Random uniform double distribution in [0, 10)
+  std::uniform_real_distribution<> cluster_dis(0, 100); // Random uniform double distribution in [0, 10)
   std::normal_distribution<> node_dis(0, sd);
 
   std::vector<std::pair<double,double>> clusters(c);
@@ -61,7 +67,7 @@ Graph clustered_graph(std::ofstream &file, int n, int c, int sd, int size = 100)
     double x = cluster.first + node_dis(gen);
     double y = cluster.second + node_dis(gen);
     nodes[i] = { x, y };
-    file << x << ' ' << y << '\n';
+    file << x << ", " << y << '\n';
   }
 
   Graph graph(n, std::vector<double>(n));
@@ -74,14 +80,17 @@ Graph clustered_graph(std::ofstream &file, int n, int c, int sd, int size = 100)
   return graph;
 }
 
-Graph clustered_and_uniform_graph(std::ofstream &file, int n, int c, int sd, int size = 100) {
+Graph clustered_and_uniform_graph(int n, int c, int sd) {
   assert(n >= 3);
   assert(c >= 2);
-  file << "Clustered and uniform graph, " << n << " nodes, " << c << " clusters, " << sd << " stdv\n";
+  char file_name[64];
+  sprintf(file_name, "./out/graph_m_n%d_c%d_sd%d.csv", n, c, sd);
+  std::ofstream file(file_name);
+  file << "x,y\n";
 
   std::random_device rd;  // Will be used to obtain a seed for the random number engine
   std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
-  std::uniform_real_distribution<> dis(0, size); // Random uniform double distribution in [0, 10)
+  std::uniform_real_distribution<> dis(0, 100); // Random uniform double distribution in [0, 10)
   std::normal_distribution<> node_dis(0, sd);
 
   std::vector<std::pair<double,double>> clusters(c);
@@ -95,13 +104,13 @@ Graph clustered_and_uniform_graph(std::ofstream &file, int n, int c, int sd, int
     double x = cluster.first + node_dis(gen);
     double y = cluster.second + node_dis(gen);
     nodes[i] = { x, y };
-    file << x << ' ' << y << '\n';
+    file << x << ", " << y << '\n';
   }
   for (; i < n; ++i) {
     double x = dis(gen);
     double y = dis(gen);
     nodes[i] = { x, y };
-    file << x << ' ' << y << '\n';
+    file << x << ", " << y << '\n';
   }
 
   Graph graph(n, std::vector<double>(n));
--- src/test_suite.cpp
+++ src/test_suite.cpp
@@ -10,20 +10,8 @@
 #define C 10
 #define BRUTE_LIMIT 10
 
-using namespace std::chrono;
-
-void print_time(const high_resolution_clock::time_point &before) {
-  auto after = high_resolution_clock::now();
-  auto time_span = duration_cast<duration<double>>(after - before);
-  printf("    Time: %fs\n", time_span.count());
-}
-
 int main() {
   srand(time(NULL));
-  std::ofstream uni_g_file("./out/uniform_11.graph");
-  Graph uniform_g = uniform_graph(uni_g_file, 11);
-  std::ofstream cluster_g_file("./out/clustered_11_3.graph");
-  Graph clusted_g = clustered_graph(cluster_g_file, 11, 3, 3);
-  std::ofstream cluster_uni_g_file("./out/uniform_11_3.graph");
-  Graph clustered_uniform_g = clustered_and_uniform_graph(cluster_uni_g_file, 11, 3, 3);
+  Graph clustered_uniform_g = clustered_and_uniform_graph(1000, 10, 3);
+  auto s = simulated_annealing(clustered_uniform_g, 3, 'm');
 }
